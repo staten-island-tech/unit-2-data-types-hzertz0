58f6265c16293e251cf70b32f755d5fb7285feed
hzertz0 <darwenzhu1221@gmail.com>
2025-02-04 10:14:58 -0500

    bill

---
+++ app.py
@@ -0,0 +1,12 @@
+"""day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")"""
+
+bill = input("Your bill will be $100. ")
+if bill == "bad":
+
+else:
+    print("incorrect")
+

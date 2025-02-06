9b6903cd9cc166ffbc3ca376c4d1e59411327354
hzertz0 <darwenzhu1221@gmail.com>
2025-02-06 10:12:33 -0500

    Random Number cleaned up

--- number.py
+++ number.py
@@ -1,18 +1,11 @@
 import random
-
 def get_random_number():
-    return random.randint(1, 100)  # Function to get a random number
-
-# Generate the random number outside the loop, so it doesn't change after each guess
+    return random.randint(1, 100)
 randomnumber = get_random_number()
-
 while True:
-    # Ask the user to guess the number
-    randominput = int(input("Guess the number between 1 and 100: "))  # Convert input to integer
-    
-    # Check if the guess is correct
+    randominput = int(input("Guess the number between 1 and 100: "))
     if randominput == randomnumber:
         print("You guessed correctly!")
-        break  # Exit the loop if the guess is correct
+        break
     else:
-        print("Try again!")  # Prompt user to try again
+        print("Try again!")

c6526720ff4955f26701fcb2f1f8cbd6c4f90e9f
hzertz0 <darwenzhu1221@gmail.com>
2025-02-07 10:07:31 -0500

    Madlibs and Number Tries

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+print(f"One day, I decided to visit the zoo. As soon as I walked in, I saw an ANIMAL. It looked very ADJECTIVE and was making a lot of NOUN. I quickly walked over to see the ANIMAL, which was VERB in its cage. I quickly decided to VERB and then go check out the ANIMAL2 cage. He was very ADJECTIVE2 and was VERB3 a lot. I had a VERB time at the zoo!")
+print("Insert your own words in the placeholders above to create your own madlib!")
+
+animal = input("Enter the first animal: ")
+adjective = input("Enter the adjective: ")
+noun = input("Enter the noun: ")
+verb = input("Enter the verb: ")
+verb2 = input("Enter the second verb: ")
+animal2 = input("Enter the second animal: ")
+adjective2 = input("Enter the second adjective: ")
+verb3 = input("Enter the third verb: ")
+verb4 = input("Enter the fourth verb: ")
+print(f"One day, I decided to visit the zoo. As soon as I walked in, I saw an {animal}. It looked very {adjective} and was making a lot of {noun}. I quickly walked over to see the {animal}, which was {verb} in its cage. I quickly decided to {verb2} and then go check out the {animal2} cage. He was very {adjective2} and was {verb3} a lot. I had a {verb4} time at the zoo!")
--- number.py
+++ number.py
@@ -1,11 +1,17 @@
 import random
+
 def get_random_number():
     return random.randint(1, 100)
+
+Tries = 0
+
 randomnumber = get_random_number()
+print(randomnumber)
 while True:
     randominput = int(input("Guess the number between 1 and 100: "))
     if randominput == randomnumber:
-        print("You guessed correctly!")
+        print("You guessed correctly! Tries: ", Tries)
         break
     else:
         print("Try again!")
+        Tries += 1

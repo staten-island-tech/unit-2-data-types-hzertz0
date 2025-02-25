a408dd13033f303d6ef764b3f67313d7ce6bceb3
hzertz0 <darwenzhu1221@gmail.com>
2025-02-25 10:17:06 -0500

    notes

--- gcf.py
+++ gcf.py
@@ -4,10 +4,10 @@ def find_gcf(num1, num2):
     
     # Iterate from 1 to the smaller number
     for i in range(1, smallernumber + 1):
-        # Check if i is a factor of both num1 and num2
+        # check if i is a factor of both num1 & num2
         if num1 % i == 0 and num2 % i == 0:
-            gcf = i  # Runs the loop until it finds that i is the gcf of both num1 and num2
-    return gcf
+            gcf = i  # runs the loop until it finds that i is the gcf of both num1 and num2
+    return gcf # returns and changes the value of gcf once it finds a proper gcf
 
 # Get user input for the two numbers
 num1 = int(input("Enter the first number: "))

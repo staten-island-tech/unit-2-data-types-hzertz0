e19b605dc38f66d3ea7fdaf0b049dfc337bd5b56
hzertz0 <darwenzhu1221@gmail.com>
2025-02-25 10:04:01 -0500

    robux

--- gcf.py
+++ gcf.py
@@ -1,4 +1,4 @@
-"""def find_gcf(num1, num2):
+def find_gcf(num1, num2):
     # Find the smaller number between num1 and num2
     smallernumber = min(num1, num2)
     
@@ -6,7 +6,7 @@
     for i in range(1, smallernumber + 1):
         # Check if i is a factor of both num1 and num2
         if num1 % i == 0 and num2 % i == 0:
-            gcf = i  # Update the GCF
+            gcf = i  # Runs the loop until it finds that i is the gcf of both num1 and num2
     return gcf
 
 # Get user input for the two numbers
@@ -15,9 +15,5 @@ num2 = int(input("Enter the second number: "))
 
 # Find the GCF of the two numbers
 result = find_gcf(num1, num2)
-print(f"The GCF of {num1} and {num2} is: {result}")"""
+print(f"The GCF of {num1} and {num2} is: {result}")
 
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
---
+++ test.py
@@ -0,0 +1,6 @@
+def skins(money, cost, isAvailable):
+    if money >= cost and isAvailable ==True:
+        print("yay")
+    else:
+        print("no")
+skins(10, 10, True)
